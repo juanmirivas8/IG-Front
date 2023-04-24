a55ee0ce2be4217626a15be84ef00b4780ad7b7d
Juan Rivas Velasco <juanmirivas8@gmail.com>
2023-04-24 22:29:44 +0200

    main routes added to sidenav

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -7,31 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  mostrar: Boolean=false;
-  mensaje: String="Innovation Group";
-  mensaje_enlace:String ="Mostrar";
+  appName: String="INNOVATION GROUP";
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  mostrarOcultar(){
-    if(this.mostrar){
-      this.mostrar=false;
-      this.mensaje_enlace="Mostrar"
-    }else{
-      this.mostrar=true;
-      this.mensaje_enlace="Ocultar"
-    }
-
-  }
-
-  abrirPopup(job?:any) {
-
-    const data = job?{mode:'Edit',job:job}:{mode:'Create'};
-
   }
 
 }
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -14,6 +14,7 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
 import {TranslateModule} from "@ngx-translate/core";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -34,6 +35,7 @@ import {TranslateModule} from "@ngx-translate/core";
     MatSidenavModule,
     MatListModule,
     TranslateModule,
+    MatButtonModule,
   ]
 })
 export class MainModule { }
