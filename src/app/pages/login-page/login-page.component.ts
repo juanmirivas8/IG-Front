360228f7a535ced22084978052f6f34a084b5e34
Juan Rivas Velasco <juanmirivas8@gmail.com>
2023-05-04 19:59:33 +0200

    login styles half way

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginPageComponent implements OnInit {
 
-  hide = true;
   constructor() { }
 
   ngOnInit(): void {
